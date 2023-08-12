e14540af72a728e831e5bc999846f176670d6a93
Tiwari2209 <90821997+Tiwari2209@users.noreply.github.com>
2023-08-12 11:56:04 +0530

    Add files via upload

---
+++ insertion.java
@@ -0,0 +1,26 @@
+public class insertion {
+    public static void insertionSort(int arr[]){
+        for(int i = 0;i<arr.length-1;i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev + 1] = arr[prev];
+                prev--;
+            }
+            //insertion
+            arr[prev + 1] = curr;
+        }
+    }
+    public static void printArr(int arr[]){
+        for(int i = 0; i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[] = {23,54,12,76,99};
+        insertionSort(arr);
+        printArr(arr);
+    }
+    
+}
